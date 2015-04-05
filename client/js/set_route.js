74d2b14feeba53735b2850ca0a8715b194d7ad7c
idoivri <idoivri@gmail.com>
2015-04-05 04:02:16 +0300

    add better clear functionality + fix reverse functionality

--- client/js/set_route.js
+++ client/js/set_route.js
@@ -1,18 +1,31 @@
 Session.setDefault('sort-by', KnowGo.sortby.ECO);
 
-function clearForm()
-{
-    $(':input').not(':button, :submit, :reset, :hidden, :checkbox, :radio').val('');
-}
+function clearFromBox() {
+    console.log("got into clear From function!");
+    $('input[type=search].from-location').val('');
+    Session.set('from',"");
+};
+
+function clearToBox() {
+    console.log("got into clear To function!");
+    $('input[type=search].to-location').val('');
+    Session.set('to',"");
+};
 
 function reverseForm()
 {
-    var toVal = $('input[type=text].to-location').val();
-    var fromVal = $('input[type=text].from-location').val();
-    $('input[type=text].to-location').val(fromVal);
-    $('input[type=text].from-location').val(toVal);
-    
-}
+    var toVal = $('input[type=search].to-location').val();
+    var fromVal = $('input[type=search].from-location').val();
+    console.log("to:"+toVal);
+    console.log("from:"+fromVal);
+    var from = Session.get('from');
+    var to = Session.get('to');
+    $('input[type=search].to-location').val(fromVal);
+    $('input[type=search].from-location').val(toVal);
+    //reverse to/from in the session as well - first clear then re-set:
+    Session.set('to',from);
+    Session.set('from',to);    
+};
 
 Template.setRoute.rendered = function () {
     toggleCircle("ecology");
@@ -37,15 +50,15 @@ Template.setRoute.events({
         setAutoComplete();
         removeMissingInputError($(jQueryEvent.currentTarget));
     },
-    'click .clear-btn': function (jQueryEvent, BlazeTemplateInstance) {
-    
-        //console.log('the clear button was clicked');
-        clearForm();
-    },
     'click .reverse-btn': function (jQueryEvent, BlazeTemplateInstance) {
-
         //console.log('the reverse button was clicked');
         reverseForm();
+    },
+    'click .glyphicon-from': function (jQueryEvent, BlazeTemplateInstance) {
+        clearFromBox();
+    },
+    'click .glyphicon-to': function (jQueryEvent, BlazeTemplateInstance) {
+        clearToBox();
     },
     'click .circle': circleClickHandler,
     'click .lang' : changeLanguage
