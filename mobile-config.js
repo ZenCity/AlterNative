6f6b95709c777c4bfb81c1728c401b95d1e1bf27
idoivri <idoivri@gmail.com>
2015-05-02 10:29:12 +0300

    fix config and splash screens

--- mobile-config.js
+++ mobile-config.js
@@ -1,10 +1,10 @@
 App.info({
-  name: 'Local Market',
+  name: 'AlterNative',
   description: 'A recipe sharing app made in Meteor.',
-  author: 'Percolate Studio Team',
-  email: 'us@percolatestudio.com',
-  website: 'http://percolatestudio.com',
-  version: '0.0.1'
+  author: 'The ZenCity Team',
+  email: 'ido@ZenCity.io',
+  website: 'http://zencity.io',
+  version: '1.0. '
 });
 
 App.icons({
@@ -32,14 +32,14 @@ App.launchScreens({
   //'ipad_landscape_2x': 'resources/splash/splash-1024x768@2x.png',
 
   // Android
-  //'android_ldpi_portrait': 'resources/splash/splash-200x320.png',
-  //'android_ldpi_landscape': 'resources/splash/splash-320x200.png',
-  //'android_mdpi_portrait': 'resources/splash/splash-320x480.png',
-  //'android_mdpi_landscape': 'resources/splash/splash-480x320.png',
-  //'android_hdpi_portrait': 'resources/splash/splash-480x800.png',
-  //'android_hdpi_landscape': 'resources/splash/splash-800x480.png',
-  //'android_xhdpi_portrait': 'resources/splash/splash-720x1280.png',
-  //'android_xhdpi_landscape': 'resources/splash/splash-1280x720.png'
+  'android_ldpi_portrait': 'resources/splash/splash-200x320.png',
+  'android_ldpi_landscape': 'resources/splash/splash-320x200.png',
+  'android_mdpi_portrait': 'resources/splash/splash-320x480.png',
+  'android_mdpi_landscape': 'resources/splash/splash-480x320.png',
+  'android_hdpi_portrait': 'resources/splash/splash-480x800.png',
+  'android_hdpi_landscape': 'resources/splash/splash-800x480.png',
+  'android_xhdpi_portrait': 'resources/splash/splash-720x1280.png',
+  'android_xhdpi_landscape': 'resources/splash/splash-1280x720.png'
 });
 
 App.setPreference('StatusBarOverlaysWebView', 'false');
