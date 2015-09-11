1027e23b492a68d6f138acc1d2309cd8d0b6109c
Shai Efrati <shaief@gmail.com>
2015-09-11 19:17:25 +0300

    Remove drawing of shuttle stations.

--- client/views/map/map.js
+++ client/views/map/map.js
@@ -176,11 +176,6 @@ Template.map.rendered = function() {
             console.log(maslul);
             var newMaslul = cutGeoJson(PNR.to_lon, PNR.to_lat, maslul);
             console.log(maslul);
-            map.data.addGeoJson(PNR['park']['selectedStation']);
-            var sStations = PNR['park']['stations'];
-            for (var j=0; j <= sStations.length; j++){
-                map.data.addGeoJson(sStations[j]);
-            }
             map.data.addGeoJson(newMaslul);
             // Set the stroke width and color for each shuttle line
             var line_color = maslul['properties']['colorEn'];
