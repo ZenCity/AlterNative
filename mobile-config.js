47739c5520d14564258002fbeffd703a9ac0ecb9
idoivri <idoivri@gmail.com>
2015-09-13 17:28:56 +0300

    add icons and splash-screen for iOS dev & revise mobile-config.js

--- mobile-config.js
+++ mobile-config.js
@@ -2,17 +2,18 @@ App.info({
   name: 'AlterNative',
   description: 'Alternative is an app for urban transportation decision making.',
   author: 'The ZenCity Team',
-  email: 'ido@ZenCity.io',
+  email: 'ido@zencity.io',
   website: 'http://zencity.io',
-  version: '1.0.2'
+  version: '1.1.2'
 });
 
 App.icons({
   // iOS
-//  'iphone': 'resources/icons/icon-60x60.png',
- // 'iphone_2x': 'resources/icons/icon-60x60@2x.png',
-  //'ipad': 'resources/icons/icon-72x72.png',
-  //'ipad_2x': 'resources/icons/icon-72x72@2x.png',
+  'iphone': 'resources/icons-apple/icon-60x60.png',
+  'iphone_2x': 'resources/icons-apple/icon-60x60@2x.png',
+  'iphone_3x': 'resources/icons-apple/icon-60x60@3x.png',
+  //'ipad': 'resources/icons/icon-76x76.png',
+  //'ipad_2x': 'resources/icons/icon-76x76@2x.png',
 
   // Android
   'android_ldpi': 'resources/icons/icon-36x36.png',
@@ -23,13 +24,16 @@ App.icons({
 
 App.launchScreens({
   // iOS
- // 'iphone': 'resources/splash/splash-320x480.png',
- // 'iphone_2x': 'resources/splash/splash-320x480@2x.png',
-  //'iphone5': 'resources/splash/splash-320x568@2x.png',
-  //'ipad_portrait': 'resources/splash/splash-768x1024.png',
-  //'ipad_portrait_2x': 'resources/splash/splash-768x1024@2x.png',
-  //'ipad_landscape': 'resources/splash/splash-1024x768.png',
-  //'ipad_landscape_2x': 'resources/splash/splash-1024x768@2x.png',
+  'iphone': 'resources/splash-apple/splash-320x480.png',
+  'iphone_2x': 'resources/splash-apple/splash-320x480@2x.png',
+  'iphone5': 'resources/splash-apple/splash-320x568@2x.png',
+  'iphone6': 'resources/splash-apple/splash-375x667@2x.png',
+  'iphone6p_portrait': 'resources/splash-apple/splash-414x736@3x.png',
+  'iphone6p_landscape': 'resources/splash-apple/splash-736x414@3x.png',
+  // 'ipad_portrait': 'resources/splash/splash-768x1024.png',
+  // 'ipad_portrait_2x': 'resources/splash/splash-768x1024@2x.png',
+  // 'ipad_landscape': 'resources/splash/splash-1024x768.png',
+  // 'ipad_landscape_2x': 'resources/splash/splash-1024x768@2x.png',
 
   // Android
   'android_ldpi_portrait': 'resources/splash/splash-200x320.png',
@@ -44,4 +48,6 @@ App.launchScreens({
 
 App.setPreference('StatusBarOverlaysWebView', 'false');
 App.setPreference('StatusBarBackgroundColor', '#000000');
+App.setPreference('BackupWebStorage','local');
 
+App.accessRule('*');
